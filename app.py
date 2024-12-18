2a84e169d81dbad3f260c1af06240b1a30c195c3
Usman Ahamed <ahmeduzman432@gmail.com>
2024-12-18 10:14:08 +0530

    new

--- app.py
+++ app.py
@@ -1,48 +1,42 @@
 import json
 import pickle
 
-from flask import Flask,request,app,jsonify,url_for,render_template
+from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
-import pandas as pd 
+import pandas as pd
 
 app = Flask(__name__)
-### Load the model 
-reg_model = pickle.load(open('regression_model.pkl', 'rb'))
-scaler = pickle.load(open('scalling.pkl', 'rb'))
+### Load the model
+reg_model = pickle.load(open("regression_model.pkl", "rb"))
+scaler = pickle.load(open("scalling.pkl", "rb"))
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@app.route('/predict_api', methods = ['POST'])
 
+@app.route("/predict_api", methods=["POST"])
 def predict_api():
-    data = request.json['data']
+    data = request.json["data"]
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    print(np.array(list(data.values())).reshape(1, -1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
     output = reg_model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/predict', methods = ['POST'])
 
+@app.route("/predict", methods=["POST"])
 def predict():
     data = [float(x) for x in request.form.values()]
-    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
     print(final_input)
     output = reg_model.predict(final_input)[0]
-    return render_template("home.html", prediction_text = "The Predicted targete is {}".format(output))
+    return render_template(
+        "home.html", prediction_text="The Predicted targete is {}".format(output)
+    )
+
 
 if __name__ == "__main__":
-    app.run(debug= True)
-    
-    
-    
-
-    
-
-
-
-
-
+    app.run(debug=True)
